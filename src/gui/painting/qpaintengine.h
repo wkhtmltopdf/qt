179f22c742bf539b1377bc796406da7b183f61d0
Jakob Truelsen <antialize@gmail.com>
2009-09-20 12:06:52 +0200

    Add support for links, hyperlinks and outlines to the pdf print system

--- src/gui/painting/qpaintengine.h
+++ src/gui/painting/qpaintengine.h
@@ -45,6 +45,7 @@
 #include <QtCore/qnamespace.h>
 #include <QtCore/qobjectdefs.h>
 #include <QtCore/qscopedpointer.h>
+#include <QtCore/qurl.h>
 #include <QtGui/qpainter.h>
 
 QT_BEGIN_HEADER
@@ -161,6 +162,10 @@ public:
 
     virtual void drawRects(const QRect *rects, int rectCount);
     virtual void drawRects(const QRectF *rects, int rectCount);
+
+    virtual void addHyperlink(const QRectF &r, const QUrl &url) {Q_UNUSED(r); Q_UNUSED(url);}
+    virtual void addAnchor(const QRectF &r, const QString &name) {Q_UNUSED(r); Q_UNUSED(name);}
+    virtual void addLink(const QRectF &r, const QString &anchor) {Q_UNUSED(r); Q_UNUSED(anchor);}
 
     virtual void drawLines(const QLine *lines, int lineCount);
     virtual void drawLines(const QLineF *lines, int lineCount);
