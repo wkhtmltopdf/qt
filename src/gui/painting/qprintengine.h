b1762747fb4d25a3149cdfbfc1c7e873a01b0c33
Antialize <antialize@gmail.com>
2012-06-07 20:36:50 +0200

    Add image printing improvments
    * Add extra raw data field to the drawImage method
    * Make the pdf printer embed raw jpeg images if supplied
    * Make webkit forward the raw images to the print engine
    * Add setting to choose the maximal dpi of an image to include
    * When possible downscale images using smooth scaling
    * Improve compression of grayscale images for pdfs

--- src/gui/painting/qprintengine.h
+++ src/gui/painting/qprintengine.h
@@ -90,8 +90,9 @@ public:
         PPK_SupportsMultipleCopies,
         PPK_PaperSize = PPK_PageSize,
 
-        PKK_UseCompression,
-
+        PPK_UseCompression,
+        PPK_ImageQuality, 
+        PPK_ImageDPI,
         PPK_CustomBase = 0xff00
     };
 
