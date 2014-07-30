b359931086b6e1fa616819f9a91e895f96d1195a
Hoang Nguyen <hoang@webmarketingexperts.com.au>
2014-07-30 16:41:10 +1000

    Add support for validation and onblur javascript as well as alignment for Text and Combobox form control Use attributes data-acroform-validation and data-acroform-on-blur to specify javascript to be run. Use attribute data-align (left / center or right) to control alignment

--- src/gui/painting/qpainter.h
+++ src/gui/painting/qpainter.h
@@ -455,11 +455,11 @@ public:
     inline void addLink(const QRect &r, const QString &anchor);
     void addLink(const QRectF &r, const QString &anchor);
 
-    void addHiddenField(const QRectF &r, const QString &value="", const QString &name="");
-    void addTextField(const QRectF &r, const QString &text="", const QString &name="", bool multiLine=false, bool password=false, bool readOnly=false, int maxLength=-1);
-    void addCheckBox(const QRectF &r, bool checked=false, const QString &name="", bool readOnly=false);
-    void addRadioButton(const QRectF &r, const QString & group="",  bool checked=false, const QString &name="", bool readOnly=false);
-    void addComboBox(const QRectF &r, const QString &name="", const QString &option_list="[]", const QString &default_value="", bool readOnly=false);
+    void addHiddenField(const QRectF &r, const QMap<QString, QString> &data, const QString &value="", const QString &name="");
+    void addTextField(const QRectF &r, const QMap<QString, QString> &data, const QString &text="", const QString &name="", bool multiLine=false, bool password=false, bool readOnly=false, int maxLength=-1);
+    void addCheckBox(const QRectF &r, const QMap<QString, QString> &data, bool checked=false, const QString &name="", bool readOnly=false);
+    void addRadioButton(const QRectF &r, const QMap<QString, QString> &data, const QString & group="",  bool checked=false, const QString &name="", bool readOnly=false);
+    void addComboBox(const QRectF &r, const QMap<QString, QString> &data, const QString &name="", const QString &option_list="[]", const QString &default_value="", bool readOnly=false);
 
     inline void addHyperlink(int x, int y, int w, int h, const QUrl &url);
     inline void addHyperlink(const QRect &r, const QUrl &url);
