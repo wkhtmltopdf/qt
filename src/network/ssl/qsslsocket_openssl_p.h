dc1a9ad123aadaa15637b20fb199a05f0b1ac240
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2018-05-05 16:23:16 +0530

    apply debian patch to support compilation with OpenSSL 1.1
    Taken from debian/patches/openssl_1.1.patch (4:4.8.7+dfsg-13)

--- src/network/ssl/qsslsocket_openssl_p.h
+++ src/network/ssl/qsslsocket_openssl_p.h
@@ -84,6 +84,10 @@
 #include <openssl/tls1.h>
 #endif
 
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#define OPENSSL_NO_SSL2
+#endif
+
 #if OPENSSL_VERSION_NUMBER >= 0x10000000L
 typedef _STACK STACK;
 #endif
